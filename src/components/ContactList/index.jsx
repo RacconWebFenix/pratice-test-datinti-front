1b19b375c262940e26533ae279518315cdb9d58b
Marcos César Domingues <dfenixweb@gmail.com>
2024-02-26 14:36:32 -0300

    remove apply

--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -2,13 +2,44 @@ import { Box, Container, List } from "@mui/material";
 import { useEffect, useState } from "react";
 import { api } from "../../Api/api";
 import ContactItem from "../ContactItem";
+import PropTypes from "prop-types";
 
-function ContactList() {
+function ContactList({ filtredData, clearName, clearPhone, setFilter }) {
   const [contacts, setContacts] = useState([]);
 
+  const getData = async () => {
+    try {
+      await api.get("contato").then((response) => setContacts(response.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const removeContact = async (id) => {
+    try {
+      await api.delete(`contato/${id}`);
+      // fazer tratativa de sucesso!
+      await api.get("contato").then((response) => setContacts(response.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const apllyFilter = () => {
+    filtredData.length === 0 ? getData() : setContacts(filtredData);
+  };
+
+  const clearFilters = () => {
+    clearName || clearPhone ? getData() : setFilter([]);
+  };
+
   useEffect(() => {
-    api.get("contato").then((response) => setContacts(response.data));
-  }, []);
+    apllyFilter();
+  }, [filtredData]);
+
+  useEffect(() => {
+    clearFilters();
+  }, [clearName, clearPhone, setFilter]);
 
   return (
     <Box>
@@ -19,7 +50,11 @@ function ContactList() {
         <Container>
           {contacts.map((c, i) => (
             <div key={i}>
-              <ContactItem name={c.nome} phones={c.telefone} />
+              <ContactItem
+                name={c.nome}
+                phones={c.telefone}
+                removeContact={() => removeContact(c.id)}
+              />
             </div>
           ))}
         </Container>
@@ -29,3 +64,10 @@ function ContactList() {
 }
 
 export default ContactList;
+
+ContactList.propTypes = {
+  filtredData: PropTypes.array,
+  clearName: PropTypes.bool,
+  clearPhone: PropTypes.bool,
+  setFilter: PropTypes.func,
+};
