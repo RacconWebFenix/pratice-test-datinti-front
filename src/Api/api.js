0cc01fad9d19d3fc982b8376b7cd469803d40bd1
Marcos César Domingues <dfenixweb@gmail.com>
2024-02-27 12:16:50 -0300

    base url

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://pratice-test-davinti-production.up.railway.app/",
+  headers: {
+    Authorization: {
+      "Access-Control-Allow-Origin": "http://localhost:5173/",
+    },
+  },
 });
